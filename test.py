ded9c64d8db3a868fba252425bd3a14621dcc0e5
koyomin9xz <kaiznguyen98@gmail.com>
2020-04-23 10:11:57 +0700

    update

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from reader import QA
-model = QA('path')
+model = QA('/content/data/trainning')
 
 
 doc = "anh Vinh sinh năm 1998, sở hữu khối tài sản 350 tỷ USD. Quê nhà ở Tây Nguyên"
 
 q = 'ai sinh năm 1998 ?'
 
-answers = model.getPredictions(q,doc)
+answers = model.predict(q,doc)
 
 print(answers)
